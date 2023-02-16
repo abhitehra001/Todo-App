ac9bf3b61b1176174d585b7eb8723bd4817e7e70
Abhijeet Tehra <123630766+abhitehra001@users.noreply.github.com>
2023-02-17 03:16:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,5 +20,5 @@ mongoose.connect(process.env.MONGO_URL)
 app.listen(process.env.PORT, ()=>{
     console.log("Server started at",process.env.PORT);
 })
-
+app.use(cors());
 app.use("/todos", router);
